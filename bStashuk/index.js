da5e8e6d8682554f12e5788a718486377f44c1cc
iaa2021 <ivanovandrey45@gmail.com>
2025-03-18 13:25:11 +0200

    modified:   bStashuk/index.js

--- bStashuk/index.js
+++ bStashuk/index.js
@@ -1,2 +1,17 @@
-console.log("Hello, World!");
+console.log("Hello, World!")
 console.log("My first message!")
+c = 10
+console.log('variable c = ', c)
+b = null
+console.log('variable b = ', b) 
+Object(d = c) // init a reference
+console.log('object d = ', d)
+const someObj = {
+    a: 10,
+    r: true 
+}// another way of reference's init
+console.log('object someObj = ', someObj.a,', ', someObj.r)
+const copyOfSomeObj = someObj
+console.log('copyOfSomeObj = ', copyOfSomeObj.a,', ', copyOfSomeObj.r)
+copyOfSomeObj.k = 'abc'
+console.log('copyOfSomeObj with new k = ', copyOfSomeObj.a, copyOfSomeObj.r, copyOfSomeObj.k )
