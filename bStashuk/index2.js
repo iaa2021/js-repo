6143ec5b111297e39339b40bcfe20ff4cb41f4f7
iaa <iaa@Inspiron-7720-418ab10a.localdomain>
2025-03-29 20:10:34 +0200

    modified:   index2.js 29.03.25

--- bStashuk/index2.js
+++ bStashuk/index2.js
@@ -12,10 +12,18 @@ let myArray = [1, 2, 3]
 console.table(myArray)
 const myArray1 = new Array(1, 'a', 'abc', 4, 7)
 console.dir( myArray1)
-myArray.push('abc')
+myArray1.push('abc')
 console.log(myArray)
-myArray.unshift('xyz')
-console.log(myArray)
+myArray1.unshift('xyz')
+console.log(myArray1)
 myArray.forEach(element => { 
     console.log(element * 3)
 });
+const newArray = myArray.map((el) => {
+    return el * 10
+})
+console.log(newArray)
+// instead of arrow function anonimus function statement can be used
+// const newArray = myArray.map(function(el) {
+//   return el * 10
+//})
