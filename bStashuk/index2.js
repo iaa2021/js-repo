cbfcc378074882bfe369058f515dad0b757235d3
iaa2021 <ivanovandrey45@gmail.com>
2025-03-29 12:40:15 +0200

    modified:   index2.js 29.03.25

--- bStashuk/index2.js
+++ bStashuk/index2.js
@@ -8,6 +8,14 @@ try {
     console.log(error.message)
 }
 console.log('Continue...')
-for (let index = 0; index < 10; index++) {
-    console.log(index * index, ', ');
-}
+let myArray = [1, 2, 3]
+console.table(myArray)
+const myArray1 = new Array(1, 'a', 'abc', 4, 7)
+console.dir( myArray1)
+myArray.push('abc')
+console.log(myArray)
+myArray.unshift('xyz')
+console.log(myArray)
+myArray.forEach(element => { 
+    console.log(element * 3)
+});
