b50b68c458ba28632c259bea5f37ab0dbaa1e318
iaa2021 <ivanovandrey45@gmail.com>
2025-04-06 12:50:58 +0300

    bStashuk/module1/mod3.mjs bStashuk/module1/mod4.mjs bStashuk/my_app/

---
+++ bStashuk/module1/mod3.mjs
@@ -0,0 +1,6 @@
+const one = 1
+const two = 'two'
+function sum(a,b) {
+    return a + b
+}
+export{ one, two, sum}
---
+++ bStashuk/module1/mod4.mjs
@@ -0,0 +1,4 @@
+import { one as oneArg, two as twoArg, sum as sumArg}// renaming variables if necessary
+from './mod3.mjs'
+console.log( oneArg, twoArg)
+console.log('sum of 64.88 and 33.47 is ', sumArg(64.88, 33.47))
