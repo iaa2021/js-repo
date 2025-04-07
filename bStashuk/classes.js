5c05c55ce99405f397838151a77625412aa58169
iaa2021 <ivanovandrey45@gmail.com>
2025-04-07 21:59:28 +0300

    completed classes.js

--- bStashuk/classes.js
+++ bStashuk/classes.js
@@ -19,3 +19,12 @@ console.log(firstComment.hasOwnProperty('text'))
 const secondComment = new Comment('Second comment')
 const string = Comment.mergeComment(firstComment.text, secondComment.text)
 console.log(string)
+class NumbersArray extends Array{
+    sum(){
+        //return this.reduce((el, acc) => acc += el, 0)
+        return this.reduce((el, acc) => acc + el)//without initial value
+    }
+}
+const myArray = new NumbersArray(2, 5, 7, 9)
+console.log(myArray)
+console.log(myArray.sum())
