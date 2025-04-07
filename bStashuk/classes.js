6b768867cb6de047acf7798a21967313f9bb95fa
iaa2021 <ivanovandrey45@gmail.com>
2025-04-07 11:34:46 +0300

    modified class.js 7.4.25

--- bStashuk/classes.js
+++ bStashuk/classes.js
@@ -4,8 +4,18 @@ class Comment {
         this.votesQty = 0
     }
     upvote(){
-        this.votesQty++
+        this.votesQty ++
+    }
+    static mergeComment(first, second){
+        return `${first} ${second}`;
     }
 }
 const firstComment = new Comment('First comment')
 console.table(firstComment)
+console.table(firstComment instanceof Comment)//check if firstComment is derived from Comment
+firstComment.upvote()
+console.table(firstComment)
+console.log(firstComment.hasOwnProperty('text'))
+const secondComment = new Comment('Second comment')
+const string = Comment.mergeComment(firstComment.text, secondComment.text)
+console.log(string)
