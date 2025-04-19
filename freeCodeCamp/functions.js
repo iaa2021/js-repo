f11430f1ece917b6431ecad013896eaad5e546aa
iaa <iaa@Inspiron-7720-418ab10a.localdomain>
2025-04-19 19:10:57 +0300

    modified:   functions.js 19.4.2025

--- freeCodeCamp/functions.js
+++ freeCodeCamp/functions.js
@@ -5,3 +5,28 @@ function rangeNB(ourMax, ourMin) {
     return rangeNB1
 } // random integer in initiated range
 console.log(rangeNB(40,50)) 
+
+const MATH_CONSTANS =  {
+    PI : 3.14
+}
+Object.freeze(MATH_CONSTANS)
+try{
+    MATH_CONSTANS.PI = 99
+} catch(ex){
+    console.log(ex)
+}
+console.log(MATH_CONSTANS.PI)
+const sum = (() => {
+    return function sum(... args){
+        return args.reduce((a,b) => a + b, 0)
+    }
+})()
+console.log(sum(1, 2, 3, 4, 5))
+const LOCAL_FORECAST = {
+    today : { min : 72, max : 83 },
+    tomorrow : { min : 73.3, max : 85.7 }
+}
+function getForecast(forecast) {
+    return forecast.tomorrow.max
+}
+console.log(getForecast(LOCAL_FORECAST))
