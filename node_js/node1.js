6851a51b51ba62588b25367eee39b563134f8372
iaa2021 <ivanovandrey45@gmail.com>
2025-07-26 21:25:09 +0300

    modified:   node_js/node1.js new file:   node_js/node3.js new file:   node_js/node4.js

--- node_js/node1.js
+++ node_js/node1.js
@@ -2,7 +2,7 @@ let http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World! I am iaa.');
+  res.end('Hello World! I am iaa, this is my 1st file.');
 }).listen(8080);
 
 
