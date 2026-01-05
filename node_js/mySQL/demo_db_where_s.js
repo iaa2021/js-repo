5ab49dcefb2f1110679391e72db043c0cd109194
iaa2021 <ivanovandrey45@gmail.com>
2026-01-05 20:00:24 +0200

    where_s.js

---
+++ node_js/mySQL/demo_db_where_s.js
@@ -0,0 +1,16 @@
+let mysql = require('mysql');
+
+let con = mysql.createConnection({
+  host: "localhost",
+  user: "iaa",
+  password: "061100",
+  database: "mydb"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  con.query("SELECT * FROM customers WHERE address LIKE 'S%'", function (err, result) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
