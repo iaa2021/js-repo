5b6bd0881eb1be8ef65d0ca78a974949b5bb0cb4
iaa2021 <ivanovandrey45@gmail.com>
2026-01-05 18:50:14 +0200

    mysql insert, select, where

---
+++ node_js/mySQL/demo_db_insert_id.js
@@ -0,0 +1,17 @@
+ let mysql = require('mysql');
+
+let con = mysql.createConnection({
+  host: "localhost",
+  user: "iaa",
+  password: "061100",
+  database: "mydb"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  let sql = "INSERT INTO customers (name, address) VALUES ('Michelle', 'Blue Village 1')";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted, ID: " + result.insertId);
+  });
+});
