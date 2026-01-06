4bfa79e9242fb0f9b265ad646dd82af5328a34d2
iaa2021 <ivanovandrey45@gmail.com>
2026-01-06 18:30:08 +0200

    mysql order by

---
+++ node_js/mySQL/demo_db_orderby.js
@@ -0,0 +1,16 @@
+ let mysql = require('mysql');
+
+let con = mysql.createConnection({
+  host: "localhost",
+  user: "iaa",
+  password: "061100",
+  database: "mydb"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  con.query("SELECT * FROM customers ORDER BY name", function (err, result) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
