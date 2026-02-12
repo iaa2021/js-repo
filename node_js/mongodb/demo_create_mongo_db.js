54fdcc2604c090af0f7833d2d72009d54d364b67
iaa2021 <ivanovandrey45@gmail.com>
2026-02-12 17:36:39 +0200

    delete mongodb folder

--- node_js/mongodb/demo_create_mongo_db.js
+++
@@ -1,8 +0,0 @@
-let MongoClient = require('mongodb').MongoClient;
-let url = 'mongodb://localhost:27017/mydb';
-
-MongoClient.connect(url, (err, db) => {
-    if(err) throw err;
-    console.log('Database created.');
-    db.close();
-});
