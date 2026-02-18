89930fc0326875961a0f353f459cdd9ced08a251
iaa2021 <ivanovandrey45@gmail.com>
2026-02-18 16:21:55 +0200

    2 files query.js and new query.js

---
+++ node_js/mongo/mdb_query.js
@@ -0,0 +1,22 @@
+const { MongoClient } = require('mongodb');
+async function run(){
+    const url = 'mongodb://127.0.0.1:27017/?directConnection=true';
+    const client = new MongoClient(url, {ServerSelectionTimeoutMS : 500});
+    let query = { address : 'Park Lane 38'};
+    try{
+        await client.connect();
+        console.log('Server connected successfully.')
+        const db = client.db('mydb');
+        console.log('Using database: ', db.databaseName);
+        const coll = db.collection('customers');
+        const result = await coll.find(query).toArray((err, res) => {
+            if(err) throw err;
+        });
+        console.log(result);
+        } catch(error){ console.error("ERROR:", err); }
+        finally{ 
+            client.close();
+            console.log("Connection closed");
+        }
+    }
+    run();
